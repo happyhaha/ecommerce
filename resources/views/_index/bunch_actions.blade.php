19d1c5677670b5d59c27fe69da9bce6707a40735
Rahim <forbdruid@gmail.com>
2016-04-18 11:47:41 +0600

    fix

--- resources/views/_index/bunch_actions.blade.php
+++ resources/views/_index/bunch_actions.blade.php
@@ -5,14 +5,8 @@
                 {{ trans('ecommerce::default.actions.delete_selected') }}
             </option>
         </select>
-        <button onclick="deleteItem()" data-action="batchAction" data-url="{{ url('admin/ecommerce/'.$codename.'/batch') }}" class="btn btn-sm btn-default apply_bulk">
+        <button data-action="batchAction" data-url="{{ url('admin/ecommerce/'.$codename.'/batch') }}" class="btn btn-sm btn-default apply_bulk">
             {{ trans('ecommerce::default.actions.apply') }}
         </button>
     </div>
 </div>
-<script>
-    function deleteItem() {
-        confirm("Вы уверены удалить выбранные объекты?");
-
-    }
-</script>
